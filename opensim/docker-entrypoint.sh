438898ca2dd3a49961a224fdf0434008b08f57de
soup-bowl <code@soupbowl.io>
2023-01-29 21:18:32 +0000

    Region definable.

--- opensim/docker-entrypoint.sh
+++ opensim/docker-entrypoint.sh
@@ -19,8 +19,6 @@ if [ ! -e OpenSim.ini ]; then
     StorageProvider = \"OpenSim.Data.MySQL.dll\"
     ConectionString = \"Data Source=${MYSQL_SERVER:-db};Database=${MYSQL_DATABASE:-opensim};User ID=${MYSQL_USER:-root};Password=${MYSQL_PASSWORD};Old Guids=true;\""  >> OpenSim.ini
 	fi
-	cat OpenSim.ini
-	exit 330
 fi
 
 if [ ! -e config-include/StandaloneCommon.ini ]; then
@@ -34,12 +32,25 @@ if [ ! -e config-include/StandaloneCommon.ini ]; then
 	else
 		echo "    Include-Storage = \"config-include/storage/SQLiteStandalone.ini\"" >> config-include/StandaloneCommon.ini
 	fi
+
+	echo "[GridService]
+    StorageProvider = \"OpenSim.Data.Null.dll:NullRegionData\"
+    Region_${REGION_NAME:-Region} = \"DefaultRegion, FallbackRegion\"
+    ExportSupported = true" >> config-include/StandaloneCommon.ini
+
+	if [ -n "${GRID_WELCOME}" ]; then
+		sed -i -e "s/OpenSimulator is running!/${GRID_WELCOME}/g" config-include/StandaloneCommon.ini
+	fi
+
+	if [ -n "${GRID_NAME}" ]; then
+		sed -i -e "s/OpenSimulator Instance/${GRID_NAME}/g" config-include/StandaloneCommon.ini
+	fi
 fi
 
 if [ `find Regions -maxdepth 1 -name '*.ini' | wc -l` -eq 0 ]; then
 	echo >&2 "INFO: No region details found, pulling one together."
 
-	echo "[Foobar]
+	echo "[${REGION_NAME:-Region}]
 	RegionUUID = $(uuidgen)
 	Location = 1000,1000
 	InternalAddress = 0.0.0.0
