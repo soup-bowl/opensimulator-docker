59045d3ddf94f4edd5e6fa2c01c253354e5cfddc
Casey LP <code@soupbowl.io>
2023-01-29 00:12:45 +0000

    Only adjust configs if none rolled in.

--- opensim/docker-entrypoint.sh
+++ opensim/docker-entrypoint.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+chown -R $(id -u):$(id -g) defaults
+
+if [ ! -e OpenSim.ini ]; then
+	echo >&2 "INFO: No OpenSim configuration found, pulling one together."
+	cp defaults/OpenSim.ini OpenSim.ini
+fi
+
+if [ ! -e config-include/StandaloneCommon.ini ]; then
+	echo >&2 "INFO: No Grid Common configuration found, pulling one together."
+	cp defaults/StandaloneCommon.ini config-include/StandaloneCommon.ini
+fi
+
+if [ `find Regions -maxdepth 1 -name '*.ini' | wc -l` -eq 0 ]; then
+	echo >&2 "INFO: No region details found, pulling one together."
+	cp defaults/Regions.ini Regions/Regions.ini
+fi 
+
 exec "$@"
