fbaa712e0e190a4289a08ba483ba1d368cdfd662
soup-bowl <code@soupbowl.io>
2023-01-29 20:53:54 +0000

    Estate & region management added.

--- opensim/docker-entrypoint.sh
+++ opensim/docker-entrypoint.sh
@@ -6,6 +6,21 @@ chown -R $(id -u):$(id -g) defaults
 if [ ! -e OpenSim.ini ]; then
 	echo >&2 "INFO: No OpenSim configuration found, pulling one together."
 	cp defaults/OpenSim.ini OpenSim.ini
+
+	echo -e "\n[Estates]
+    DefaultEstateName = ${ESTATE_NAME:-Default Estate}
+    DefaultEstateOwnerName = ${ESTATE_OWNER_NAME:-Foo Bar}
+    DefaultEstateOwnerUUID = ${ESTATE_OWNER_UUID:-00000000-0000-0000-0000-000000000000}
+    DefaultEstateOwnerEMail = ${ESTATE_OWNER_EMAIL:-user@example.com}
+    DefaultEstateOwnerPassword = ${ESTATE_OWNER_PASSWORD:-password}" >> OpenSim.ini
+
+	if [[ "${DATABASE_ENGINE}" == "mysql" ]]; then
+		echo -e "\n[Messaging]
+    StorageProvider = \"OpenSim.Data.MySQL.dll\"
+    ConectionString = \"Data Source=${MYSQL_SERVER:-db};Database=${MYSQL_DATABASE:-opensim};User ID=${MYSQL_USER:-root};Password=${MYSQL_PASSWORD};Old Guids=true;\""  >> OpenSim.ini
+	fi
+	cat OpenSim.ini
+	exit 330
 fi
 
 if [ ! -e config-include/StandaloneCommon.ini ]; then
@@ -19,12 +34,18 @@ if [ ! -e config-include/StandaloneCommon.ini ]; then
 	else
 		echo "    Include-Storage = \"config-include/storage/SQLiteStandalone.ini\"" >> config-include/StandaloneCommon.ini
 	fi
-	cat config-include/StandaloneCommon.ini
 fi
 
 if [ `find Regions -maxdepth 1 -name '*.ini' | wc -l` -eq 0 ]; then
 	echo >&2 "INFO: No region details found, pulling one together."
-	cp defaults/Regions.ini Regions/Regions.ini
+
+	echo "[Foobar]
+	RegionUUID = $(uuidgen)
+	Location = 1000,1000
+	InternalAddress = 0.0.0.0
+    InternalPort = 9000
+    AllowAlternatePorts = False
+    ExternalHostName = localhost" >> Regions/Regions.ini
 fi 
 
 exec "$@"
